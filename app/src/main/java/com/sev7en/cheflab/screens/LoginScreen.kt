ece32a14998e8fb96ee49496e5f9273f03361500
Mahaveer prasad panigrahi <psatyam86619@gmail.com>
2023-07-29 15:27:55 +0530

    Made Login UI

---
+++ app/src/main/java/com/sev7en/cheflab/screens/LoginScreen.kt
@@ -0,0 +1,12 @@
+package com.sev7en.cheflab.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sev7en.cheflab.R
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
---
+++ app/src/main/java/com/sev7en/cheflab/screens/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.sev7en.cheflab.screens
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sev7en.cheflab.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
